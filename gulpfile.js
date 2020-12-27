93ce2b91c7b2ae458f9255926d9fbebd369db5ed
Yura Korniyenko <yura.kornssnko@gmail.com>
2020-12-28 01:20:55 +0200

    Optimization

--- gulpfile.js
+++ gulpfile.js
@@ -178,9 +178,6 @@ function fonts() {
   src(path.src.fonts)
     .pipe(ttf2woff())
     .pipe(dest(path.build.fonts));
-  return src(path.src.fonts)
-    .pipe(ttf2woff2())
-    .pipe(dest(path.build.fonts));
 }
 
 gulp.task("svgSprite", function () {
